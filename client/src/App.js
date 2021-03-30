2d28b8789ebace66c9a425348e0bff2ed03cbb77
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-03-30 19:54:39 +0530

    setup socket.io-client with context for sharing between all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
+import SocketContext from './context/socket';
 const ENDPOINT = "http://localhost:5000";
 
 function App() {
   const [response, setResponse] = useState("");
+  const [message, setMessage] = useState("");
 
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("sendMessage", data => {
-      setResponse(data);
-    });
-  }, []);
+  const socket = socketIOClient(ENDPOINT);
+  socket.on("sendMessage", data => {
+    setResponse(data);
+  });
+  // useEffect(() => {
+  // }, []);
 
   return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
+    <SocketContext.Provider value={socket}>
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+    </SocketContext.Provider>
   );
 }
 
